7933cf093b46b89f0427d9c72e91c6e08a1eb9a1
Tokkiu <mrgao.ary@gmail.com>
2016-05-17 20:22:46 +0800

    1.游戏界面自适应 2.部分样式修改

--- racegame-master/racegame-master/webview.js
+++ racegame-master/racegame-master/webview.js
@@ -1,7 +1,14 @@
 function setHtmlSize(){
 
   var windowWidth = document.documentElement.clientWidth;
+  var windowHeight=document.documentElement.clientHeight;
+  console.log(windowWidth,windowHeight);
+  document.getElementById('safe').style.width=0.8*windowWidth+'px';
+  document.getElementById('safe').style.height=0.3*windowWidth+'px';
+  document.getElementById('phone').style.height=0.3*windowWidth+'px';
 
+  document.getElementById('phone').style.width=0.8*windowWidth+'px';
+  document.getElementById('main').style.height=windowHeight+'px';
   if(windowWidth > 800){
 
     document.getElementsByTagName("html")[0].style.fontSize = 16 + "px";
